140f87cfe5d1694201607d6e1b2334bbce5afa4c
Ved Sunil Nalavade <vedsnalavade@gmail.com>
2025-09-04 21:16:00 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import { PrismaClient } from "@prisma/client";
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB!");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+  await connectMongoDB();
+
+  try {
+    // await prisma.$connect();
+    // console.log("Connected to Azure SQL via Prisma!");
+  } catch (err) {
+    console.error("Prisma connection error:", err);
+  }
+});
